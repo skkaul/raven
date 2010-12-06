5051485d64a2e252e9eedd2e9927f783d51e2d77
srikanth.saripalli@gmail.com <srikanth.saripalli@gmail.com@5da3f1cf-7a0d-2535-09dc-823283d8748c>
2010-12-06 00:14:00 +0000

    Working on the roboteq driver

--- trunk/roboteq/nodes/roboteq_node.cpp
+++ trunk/roboteq/nodes/roboteq_node.cpp
@@ -53,10 +53,21 @@
 
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
 #include <std_msgs/String.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
+
+
 
 using namespace std;
+
+//globals
+namespace {
+
+    RoboteqDevice device;
+};
 
 
 void publish_pose(tf::TransformBroadcaster *odom_broadcaster, ros::Publisher *odom_pub)
@@ -66,9 +77,81 @@ void publish_pose(tf::TransformBroadcaster *odom_broadcaster, ros::Publisher *od
   odom_pub->publish(odom_pose);
 }
 
-void cmdvelcallback(const std_msgs::String::ConstPtr& msg)
+void cmdvelcallback(const geometry_msgs::Twist::ConstPtr& cmd_vel)
 {
+  float v_mag, mag_r;
+  float vel_x, vel_y, vel_yaw;
+  float  delta_v, v_left, v_right;
+  float speed_left, speed_right;
+  int left_cmd, right_cmd;
+  
   ROS_INFO("cmd vel callback");
+
+  vel_x = cmd_vel->linear.x;
+  vel_y = cmd_vel->linear.y;
+  vel_yaw = cmd_vel->angular.z;
+
+  //Calculate magnitutde of the velocity vmag and Radius of Rotation mag_r
+  v_mag = sqrt((pow(vel_x, 2)) + pow(vel_y, 2));
+  if (vel_yaw == 0){
+    mag_r = 0;
+  }
+  else {
+    mag_r = v_mag/vel_yaw;
+  }
+        
+  //Calculate the difference in velocity between the wheels
+  if (mag_r == 0){
+    delta_v = 0;
+  }
+  else {
+    delta_v = 2.0 * ((v_mag * WHEEL_BASE_LENGTH)/mag_r);
+  }
+        
+  //Calculate the velocity of each wheel
+  if (v_mag == 0){
+    v_left = vel_yaw * -1;
+    v_right = vel_yaw;
+  }else {
+    v_left  = (v_mag - (0.5 * delta_v));
+    v_right = (v_mag + (0.5 * delta_v));
+  }
+
+  //Calculate percentage of max velocity for each wheel
+  if (v_left == 0){
+    speed_left = 0;
+  } else if ( v_left > MAX_WHEEL_VELOCITY) {
+    speed_left = 1;
+  } else if ( v_left < -MAX_WHEEL_VELOCITY) {
+    speed_left = -1;
+  } else {
+    speed_left = v_left/MAX_WHEEL_VELOCITY;
+  }
+
+  if (v_right == 0){
+    speed_right = 0;
+  } else if ( v_right > MAX_WHEEL_VELOCITY) {
+    speed_right = 1;
+  } else if ( v_right < -MAX_WHEEL_VELOCITY) {
+    speed_right = -1;
+  } else {
+    speed_right = v_right/MAX_WHEEL_VELOCITY;
+  }
+
+  left_cmd  = int(speed_left * MOTOR_RANGE);
+  right_cmd = int(speed_right * MOTOR_RANGE);
+
+  //send these to the roboteq controller
+  // TO DO
+  int status;
+  ROS_INFO("- SetCommand(_GO, 1, 1)...");
+  if((status = device.SetCommand(_GO, 1, left_cmd)) != RQ_SUCCESS)
+    cout<<"failed --> "<<status<<endl;
+  //sleep for 10 ms do we have to do this 
+  ros::Duration(0.01).sleep(); //sleep for 10 ms 
+  if((status = device.SetCommand(_GO, 2, right_cmd)) != RQ_SUCCESS)
+    cout<<"failed --> "<<status<<endl;
+
 }
 
 int main( int argc, char **argv )
@@ -87,7 +170,6 @@ int main( int argc, char **argv )
     n.param("baudrate", baudrate, 115200);
     n.param<std::string>("frame_id", frame_id, "/base_frame");
    
-    RoboteqDevice device;
      
     int status = device.Connect(portname);
 
@@ -96,7 +178,7 @@ int main( int argc, char **argv )
       return -1;
     }
     ROS_INFO("Roboteq -- Successfully connected to the Roboteq HDC2450");
-    ros::Duration(0.05).sleep(); //sleep for 50 ms 
+    ros::Duration(0.01).sleep(); //sleep for 10 ms 
     
     	
     ROS_INFO("- SetConfig(_DINA, 1, 1)...");
@@ -104,7 +186,7 @@ int main( int argc, char **argv )
       cout<<"failed --> "<<status<<endl;
     else
       ROS_INFO("succeeded.");
-    ros::Duration(0.05).sleep(); //sleep for 50 ms 
+    ros::Duration(0.01).sleep(); //sleep for 10 ms 
 
     int result;
     ROS_INFO("- GetConfig(_DINA, 1)...");
@@ -113,24 +195,10 @@ int main( int argc, char **argv )
     else
       cout<<"returned --> "<<result<<endl;
     ROS_INFO("Roboteq -- Successfull setup of the Roboteq HDC2450");
-    ros::Duration(0.05).sleep(); //sleep for 50 ms 
-
-    ROS_INFO("- GetValue(_ANAIN, 1)...");
-    if((status = device.GetValue(_ANAIN, 1, result)) != RQ_SUCCESS)
-      cout<<"failed --> "<<status<<endl;
-    else
-      cout<<"returned --> "<<result<<endl;
-    ros::Duration(0.05).sleep(); //sleep for 50 ms 
-
-    ROS_INFO("- SetCommand(_GO, 1, 1)...");
-    if((status = device.SetCommand(_GO, 1, 1)) != RQ_SUCCESS)
-      cout<<"failed --> "<<status<<endl;
-    else
-      cout<<"succeeded."<<endl;
-    ros::Duration(0.05).sleep(); //sleep for 50 ms 
+    ros::Duration(0.01).sleep(); //sleep for 10 ms 
 
     //subscribe to the motor_control topic to listen to motor commands
-    ros::Subscriber roboteq_sub = n.subscribe("cmd_vel", 1, cmdvelcallback, noDelay);
+    ros::Subscriber roboteq_sub = n.subscribe<geometry_msgs::Twist>("cmd_vel", 1, cmdvelcallback, noDelay);
 
     //setup publisher data for publishing odometry data
     ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom",1);

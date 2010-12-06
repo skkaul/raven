94effb68b97b6fb6246638cf9981bb4da435e02c
srikanth.saripalli <srikanth.saripalli@gmail.com>
2010-12-06 00:14:00 +0000

    Working on the roboteq driver

--- roboteq/include/roboteq/Raven.h
+++ roboteq/include/roboteq/Raven.h
@@ -1,9 +1,10 @@
 #ifndef __RAVEN__
 #define __RAVEN__
 
-#define WHEEL_BASE_LENGTH  = 1.2 //meters
-#define WHEEL_RADIUS       = 1.2 //meters
-#define MAX_WHEEL_VELOCITY = 1.2 //m/s
-#define MOTOR_RANGE        = 1.2 // Relative motor speed
+#define WHEEL_BASE_LENGTH  0.6 //meters
+#define WHEEL_BASE_WIDTH   0.9 //meters
+#define WHEEL_DIAMETER     1.2 //meters
+#define MAX_WHEEL_VELOCITY 1.2 //m/s
+#define MOTOR_RANGE        500 // Relative motor speed
 
 #endif
